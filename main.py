85a2b486de8be4f6fa69a5dfa657591c6e551d14
Lan Yang <ireneyanglan@gmail.com>
2015-02-13 17:40:29 -0500

    frastructure

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ import base64
 import json
 
 s = raw_input('Please input word: ')
-p = input('Please input value: ')
-bingUrl = r'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' +  s + '%27&$top=10&$format=JSON'
-#Provide your account key here
-accountKey = 'Ib4wTA9BOGjrp2Nug7wqKJN3Sjw++0u84wcBY5oN8U0'
+p = int(round(input('Please input value: ') * 10))
+precision = 1
+while (precision != 0 and precision < p):
+    bingUrl = r'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' +  s + '%27&$top=10&$format=JSON'
+    accountKey = 'Ib4wTA9BOGjrp2Nug7wqKJN3Sjw++0u84wcBY5oN8U0'
+    accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
+    headers = {'Authorization': 'Basic ' + accountKeyEnc}
+    req = urllib2.Request(bingUrl, headers = headers)
+    response = urllib2.urlopen(req)
+    content = response.read()
+    json_dict = json.loads(content)
+    precision = 0
 
-accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
-headers = {'Authorization': 'Basic ' + accountKeyEnc}
-req = urllib2.Request(bingUrl, headers = headers)
-response = urllib2.urlopen(req)
-content = response.read()
-#root = ET.fromstring(content)
-#content contains the xml/json response from Bing.
-json_dict = json.loads(content)
+    for i in range(10):
+        print json_dict['d']['results'][i]['Title'].encode('utf-8')
+        print json_dict['d']['results'][i]['Description'].encode('utf-8')
+        print json_dict['d']['results'][i]['Url'].encode('utf-8')
+        relevance = raw_input('Relevant? yes/no: ')
+        if relevance == 'yes':
+            precision = precision + 1   
+    print precision
+    print p
+    print precision < p
+        
+          
 
-for i in range(10):
-    print json_dict['d']['results'][i]['Title'].encode('utf-8')
-    print json_dict['d']['results'][i]['Description'].encode('utf-8')
-    print json_dict['d']['results'][i]['Url'].encode('utf-8')
-# print root.tag
-# print root.attrib
-# print root[6][2].text
-# for child in root:
-#     print child.tag, child.attrib
