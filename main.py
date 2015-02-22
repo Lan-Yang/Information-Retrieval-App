876599b16c91d2049ceee0f3363dfe82f0c1f0cc
Lan Yang <ireneyanglan@gmail.com>
2015-02-22 01:03:25 -0500

    done 1st

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib2
 import base64
 import json
+import re
 
 s = raw_input('Please input word: ')
 p = int(round(input('Please input value: ') * 10))
@@ -15,20 +16,32 @@ while (precision != 0 and precision < p):
     content = response.read()
     json_dict = json.loads(content)
     precision = 0
+    goodset = set()
+    badset = set()
 
     for i in range(10):
+        title = json_dict['d']['results'][i]['Title'].encode('utf-8')
+        descrip = json_dict['d']['results'][i]['Description'].encode('utf-8')
         print 'Result ' + str(i + 1)
-        print json_dict['d']['results'][i]['Title'].encode('utf-8')
-        print json_dict['d']['results'][i]['Description'].encode('utf-8')
+        print title
+        print descrip
         print json_dict['d']['results'][i]['Url'].encode('utf-8')
+        wordset = set(re.split(r'[^\w]+', descrip)) | set(re.split(r'[^\w]+', title))
+        wordset.remove('')
         relevance = raw_input('Relevant (Y/N): ')
         if relevance == 'Y':
             precision = precision + 1
-    print 'Precision\t='+ str(precision)
-    print 'Query\t=' + s
-#     print precision
-#     print p
-    #print precision < p
+            goodset = goodset | wordset
+        else:
+            badset = badset | wordset
+    wordset = goodset - badset
+    words = list(wordset)
+    if (len(words) > 0):
+        for j in words:
+            s = s + '+' + j
+    print 'Precision ='+ str(precision)
+    print 'Query =' + s
+
         
           
 
